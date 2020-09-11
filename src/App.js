26a2cfe9a4633de916e50c8e28b6f24fb39500b0
ddayooung <dimmaculatey@gmail.com>
2020-09-11 14:12:12 +0900

    redux thunk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import CounterContainer from './containers/CounterContainal';
-
-import './App.css';
+import SampleContainer from './containers/SampleContainer';
 
 function App() {
   return (
     <div className="App">
       <CounterContainer />
+      <SampleContainer />
     </div>
   );
 }
